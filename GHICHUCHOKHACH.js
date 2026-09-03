e5376d5fbc7f6c9ba6a5ec8f3deb91886464a02c
tiorepro <tiorepro.tran@gmail.com>
2026-09-03 18:16:02 +0700

    Add files via upload

---
+++ GHICHUCHOKHACH.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const printNoteKhach = document.getElementById("print-note-khach");
+    if (printNoteKhach) {
+        printNoteKhach.innerHTML = `
+        <h2>ĐẮC THIÊN KHÍ - VẠN SỰ AN</h2>
+        <p>* Đây là nội dung ngày giờ tốt đã được tìm kiếm và lựa chọn cẩn thận dựa trên nhiều phương pháp cổ học tinh hoa, kết hợp với các yếu tố Thiên - Địa - Nhân Khí để mang lại sự cát tường, hanh thông cho công việc của quý gia chủ.</p>
+        <p>** Các giờ được tô màu xanh là những khung giờ tốt đã được chọn. Xin vui lòng tuân thủ để mọi việc được thuận lợi, viên mãn.</p>
+        
+        <p>Lưu ý:</p>
+            <p>1. Ngày giờ tốt được đưa ra dưới đây là được lựa chọn theo tuổi, theo mục đích cụ thể (những tuổi khác, những mục đích khác không sử dụng).</p>
+            <p>2. Tùy vào vùng địa lý, thời gian trong năm mà thời gian mặt trời mọc, đứng bóng, lặn trong ngày sẽ khác nhau, do đó khung thời gian chuẩn sẽ có sự thay đổi. Địa điểm người xem chọn là "TP. Hồ Chí Minh", nếu có thay đổi địa điểm xem vui lòng báo lại.</p>
+            <p>3. Trong một số việc như xuất hành, đứng cúng,... để tối đa hiệu quả có thể chọn hướng "Tài thần" hoặc "Hỷ thần" để xuất phát, xoay mặt về hướng đó để cúng,...</p>
+        <p>Trân trọng.</p>
+        `;
+    }
+});
